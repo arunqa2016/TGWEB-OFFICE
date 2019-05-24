34cfb8a88a23cdbb8d6b592890b81212e039191c
DESKTOP-P2IQJOQ\HP <arun@aquacrmsoftware.com>
2019-05-24 23:00:53 +0530

    Code in Extent Report

--- src/main/java/com/tgweb/extentReportListener/ExtentReporterNG.java
+++ src/main/java/com/tgweb/extentReportListener/ExtentReporterNG.java
@@ -6,10 +6,16 @@ import org.testng.IReporter;
 import org.testng.ISuite;
 import org.testng.xml.XmlSuite;
 
+import com.relevantcodes.extentreports.ExtentReports;
+
 public class ExtentReporterNG implements IReporter {
 
+	private ExtentReports extent;
+	
 	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
 		System.out.println("Extent Report Listener.");
+		
+		
 		
 	}
 
